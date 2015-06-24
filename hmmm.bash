b3878c96740c5c66dee21d52c4d518506ab29811
James Zimmerman II <james@comfreeze.net>
2015-06-23 23:36:01 -0700

    Fixing issue with variable name used for host.

---
+++ hmmm.bash
@@ -0,0 +1,67 @@
+#!bash
+#
+# bash completion file for core hmm helpers
+#
+# To enable the completions either:
+#  - place this file in /etc/bash_completion.d
+#  or
+#  - copy this file and add the line below to your .bashrc after
+#    bash completion features are loaded
+#     . hmm.bash
+#
+
+_hmm()
+{
+    local cur prev opts base opts actions
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    PREV_CWORD=${COMP_CWORD}-1
+    prev="${COMP_WORDS[$PREV_CWORD]}"
+    HMM=$( which hmm )
+    HMM_PATH=$( $HMM --path )
+    TARDIR="$HMM_PATH/.hmm/*"
+    COMPREPLY=()
+
+    #
+    #  The basic options we'll complete.
+    #
+    opts=" \
+###MINIFY-ACTIONS### \
+ attach build cleanup commit control enter export fork get-details get-http get-https get-image get-log get-pid get-port get-source import init install kill pull push reload restart run serve snapshot spawn ssh start status stop tag \
+    -h --help \
+    -a --actions \
+    -i --info \
+    -f --force \
+    -V --verbose \
+    -c --config \
+    -H --home \
+    -p --port \
+    -v --vol \
+    -e --env \
+    -n --name \
+    -R --registry \
+    -B --base \
+    -T --tag \
+    -P --parent \
+    -u --user \
+    -C --control \
+    --upgrade \
+    --path"
+
+    #
+    #  Complete the arguments to some of the basic commands.
+    #
+    if [[ "$prev" == -* ]]; then
+        case "$prev" in
+            "-c"|"--config")
+                _filedir '@(hmm)'
+                return 0
+                ;;
+            *)
+                ;;
+        esac
+    fi
+
+    actions=$(for x in $TARDIR; do echo $(basename ${x%}); done)
+    COMPREPLY=( $( compgen -W "$opts $actions" -- "$cur" ) )
+}
+complete -F _hmm $filenames hmm
