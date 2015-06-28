da1439d1881bde2b206f107b2bfd023e633183be
James Zimmerman II <james@comfreeze.net>
2015-06-27 23:48:14 -0700

    Updating bash completion logic with better organization.

--- hmm.bash
+++ hmm.bash
@@ -9,6 +9,102 @@
 #    bash completion features are loaded
 #     . hmm.bash
 #
+PLUGDIR=""
+CURDIR=$( pwd )
+
+get_actions() {
+    TARGET=$1
+    RESULT=""
+    cd ${TARGET}/
+    for ACTION_FILE in *
+    do
+        if [[ -f ${ACTION_FILE} ]]; then
+        	[[ ${RESULT} = "" ]] || RESULT+=" "
+        	RESULT+="${ACTION_FILE}"
+        fi
+
+        if [[ -d ${ACTION_FILE} ]]; then
+            cd ${ACTION_FILE}
+            for SUB_ACTION_FILE in *
+            do
+                [[ ${RESULT} = "" ]] || RESULT+=" "
+                RESULT+="${ACTION_FILE}-${SUB_ACTION_FILE}"
+			done
+			cd ..
+		fi
+    done
+    cd ${CURDIR}
+    RETURN_HOLDER=${RESULT}
+    return
+}
+
+short_opts()
+{
+    #
+    #  The basic options we'll complete.
+    #
+    opts=" \
+    -h \
+    -i \
+    -a \
+    -U \
+    -f \
+    -V \
+    -H \
+    -c \
+    -p \
+    -l \
+    -v \
+    -e \
+    -n \
+    -R \
+    -r \
+    -B \
+    -T \
+    -t \
+    -P \
+    -o \
+    -L \
+    -u \
+    -C";
+    RETURN_HOLDER=${opts}
+    return
+}
+
+long_opts()
+{
+    #
+    #  The basic options we'll complete.
+    #
+    opts=" \
+    --help \
+    --info \
+    --actions \
+    --update \
+    --force \
+    --verbose \
+    --home \
+    --config \
+    --port \
+    --link \
+    --vol \
+    --env \
+    --name \
+    --registry \
+    --run \
+    --base \
+    --tag \
+    --test \
+    --parent \
+    --host \
+    --platform \
+    --user \
+    --control \
+    --upgrade \
+    --path";
+    RETURN_HOLDER=${opts}
+    return
+}
 
 _hmm()
 {
@@ -17,34 +113,9 @@ _hmm()
     PREV_CWORD=${COMP_CWORD}-1
     prev="${COMP_WORDS[$PREV_CWORD]}"
     HMM=$( which hmm )
-    HMM_PATH=$( $HMM --path )
-    TARDIR="$HMM_PATH/.hmm/*"
+    HMM_PATH=$( ${HMM} --path )
+    PLUGDIR="$HMM_PATH/.hmm/"
     COMPREPLY=()
-
-    #
-    #  The basic options we'll complete.
-    #
-    opts=" \
-###MINIFY-ACTIONS### \
-    -h --help \
-    -a --actions \
-    -i --info \
-    -f --force \
-    -V --verbose \
-    -c --config \
-    -H --home \
-    -p --port \
-    -v --vol \
-    -e --env \
-    -n --name \
-    -R --registry \
-    -B --base \
-    -T --tag \
-    -P --parent \
-    -u --user \
-    -C --control \
-    --upgrade \
-    --path"
 
     #
     #  Complete the arguments to some of the basic commands.
@@ -60,7 +131,20 @@ _hmm()
         esac
     fi
 
-    actions=$(for x in $TARDIR; do echo $(basename ${x%}); done)
-    COMPREPLY=( $( compgen -W "$opts $actions" -- "$cur" ) )
+    short_opts
+    SHORT=$RETURN_HOLDER
+    long_opts
+    LONG=$RETURN_HOLDER
+    #    actions=$(for x in $PLUGDIR; do echo $(basename ${x%}); done)
+    if [[ ${COMP_WORDS[COMP_CWORD]} == "-" ]]; then
+        COMPREPLY=( $( compgen -W "$SHORT $LONG" -- "$cur" ) )
+    elif [[ ${COMP_WORDS[COMP_CWORD]} == "--" ]]; then
+        COMPREPLY=( $( compgen -W "$LONG" -- "$cur" ) )
+    else
+        get_actions "${PLUGDIR}"
+        actions=${RETURN_HOLDER}
+        COMPREPLY=( $( compgen -W "$actions" -- "$cur" ) )
+    fi
 }
-complete -F _hmm $filenames hmm
+#complete -F _hmm ${filenames} hmm
+complete -F _hmm hmm
